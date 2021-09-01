5e5f836be3f5b53299c473596b70248c4ebfcd54
mshtelma <michael.shtelma@databricks.com>
2021-09-01 17:28:36 +0200

    Small update

---
+++ scripts/using_mlflow_tracking_remotely.py
@@ -0,0 +1,15 @@
+import os
+import mlflow
+mlflow.set_tracking_uri("databricks")
+os.environ['DATABRICKS_HOST'] = 'https://e2-demo-field-eng.cloud.databricks.com/'
+os.environ['DATABRICKS_TOKEN'] = ''
+
+#set experiment
+experiment_path = "/Users/Michael.Shtelma@databricks.com/experiments/leclub1"
+mlflow.set_experiment(experiment_path)
+
+# tracking
+with mlflow.start_run(run_name="Training") as run:
+    mlflow.set_tag("action", "test2222")
+    mlflow.log_metric("acc", 0.99)
+    mlflow.log_param("param", "my value")
