a3c7e0db5dbd5a4ee140f193a7791566b774c5aa
mshtelma <mshtelma>
2022-03-14 13:17:56 +0000

    Added notebook for training pipeline

---
+++ notebooks/training_pipeline_test.py
@@ -0,0 +1,22 @@
+# Databricks notebook source
+# MAGIC %pip install -r ../requirements.txt
+
+# COMMAND ----------
+
+import json
+from lendingclub_scoring.common import Job
+from lendingclub_scoring.config.ConfigProvider import setup_mlflow_config
+from lendingclub_scoring.pipelines.LendingClubTrainingPipeline import (
+    LendingClubTrainingPipeline,
+)
+
+# COMMAND ----------
+
+conf = json.load(open('../conf/lendingclub_config.json'))
+experiment_id = setup_mlflow_config(conf)
+p = LendingClubTrainingPipeline(spark, conf["data-path"], conf["model-name"])
+p.run()
+
+# COMMAND ----------
+
+
